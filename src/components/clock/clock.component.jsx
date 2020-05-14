f279364dfd045d1d0d05f1e25a1a2fccaeacfe77
Deepak Bhamla <dbhamla7@gmail.com>
2020-05-14 22:47:48 +0530

    lets deploy

--- src/components/clock/clock.component.jsx
+++ src/components/clock/clock.component.jsx
@@ -6,7 +6,7 @@ export class TypicalText extends Component {
   render () {
     return (
       <Typical
-        steps={['“Sometimes our stop-doing list needs to be bigger than our to-do list.”', 1000, '“The only thing more important than your to-do list is your to-be list.”', 1000,'“It is a waste of time not to do the most effective, useful, or important thing you can do.”',1000]}
+        steps={['“forexample”', 1000, '“meeting with client @2:20pm”', 1000,'“Watch movies at netflix @10:30pm”',1000]}
         loop={Infinity}
         wrapper="p"
       />
