4bb7496d1f4eb6f06335775e84810861381629c7
Deepak Bhamla <dbhamla7@gmail.com>
2020-05-14 21:46:24 +0530

    Updates

---
+++ precache-manifest.1fc6c162ae0e5b325d1c7aa5aaf06348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cdf0d07464a5ceda92d1cc3bb47798",
+    "url": "/todo-deploy/index.html"
+  },
+  {
+    "revision": "9063b658a9488da11080",
+    "url": "/todo-deploy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c420c7c2c9ffc8565e7b",
+    "url": "/todo-deploy/static/css/main.f06540ac.chunk.css"
+  },
+  {
+    "revision": "9063b658a9488da11080",
+    "url": "/todo-deploy/static/js/2.6d3becaa.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/todo-deploy/static/js/2.6d3becaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c420c7c2c9ffc8565e7b",
+    "url": "/todo-deploy/static/js/main.64827e42.chunk.js"
+  },
+  {
+    "revision": "86b4109bf822f5a1288d",
+    "url": "/todo-deploy/static/js/runtime-main.17ab04ba.js"
+  }
+]);
